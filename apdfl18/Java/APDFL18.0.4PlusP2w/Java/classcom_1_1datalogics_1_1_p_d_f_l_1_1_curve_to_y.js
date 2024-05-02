1a37e3021f21f27826ea871588e7601ade16ba51
bruceh <bruceh@datalogics.com>
2024-05-02 16:18:49 -0500

    Add .NET, .NETFramework, and Java documentation for APDFL18.0.4PlusP2w

---
+++ apdfl18/Java/APDFL18.0.4PlusP2w/Java/classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.js
@@ -0,0 +1,16 @@
+var classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y =
+[
+    [ "CurveToY", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a212f4d232b2318266a01d3fe5390e444", null ],
+    [ "CurveToY", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a9d728fecee6e99b2601416c3cabdabbd", null ],
+    [ "CurveToY", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#ae1cc154f8de5fc017446b0b02e2e719f", null ],
+    [ "delete", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a077d8980c3a152e5248bf73fac11ca98", null ],
+    [ "delete", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a0a3b1e80905dc538c890f189b2267663", null ],
+    [ "finalize", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a7621a5c61ca0e2e4e72faf0f66546217", null ],
+    [ "getCPtr", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a54e17bde63eedd8fc94a0262d7091286", null ],
+    [ "getCPtr", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a6d2fa78d13a8a0a260032f77573cf640", null ],
+    [ "getPoint1", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a4da64cc399d650f5531e550ecd085c29", null ],
+    [ "getPoint3", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a7201a7bc730d1a97767e09fe167668c2", null ],
+    [ "setPoint1", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a93fa2ac435e39e3e0cebf41f0c6e1fed", null ],
+    [ "setPoint3", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a86216398ddc7105379eee5aff3275c9b", null ],
+    [ "toString", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_curve_to_y.html#a2af6b2049550bb91c288cb773b82d9d2", null ]
+];
---
+++ apdfl18/Java/APDFL18.0.4PlusP2w/Java/classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.js
@@ -0,0 +1,20 @@
+var classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label =
+[
+    [ "PageLabel", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a09a351d643ac5de27b83c0ea50370971", null ],
+    [ "PageLabel", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a781a372f4027a22232362724b83c8d4e", null ],
+    [ "PageLabel", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#aa9972216bdca4a7e0d2c0be52280aa5c", null ],
+    [ "delete", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#afb77deb001a8af2ee8f96912209c97b3", null ],
+    [ "delete", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a7fb1a12e48865ea6ae94e979da687386", null ],
+    [ "finalize", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a99d7017cd7e677028f26b90af4c127ac", null ],
+    [ "getCPtr", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a50f0345f6d8145a2594c80bff046d36c", null ],
+    [ "getEndPageIndex", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a5c457fc6821d12696d9f37051a856bad", null ],
+    [ "getFirstNumberInRange", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#aeeb7df877cf003ddc470ebe409a79424", null ],
+    [ "getNumberStyle", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a406f04c3a5c817dd0f0ca0927fe7686a", null ],
+    [ "getPrefix", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#aff6e688dcc3a01163456472534456ced", null ],
+    [ "getStartPageIndex", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a5f4b97e3f53c5978fda6ade86e9c0840", null ],
+    [ "setFirstNumberInRange", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a93bbcef410854ebbe7df51d4c0b7a6a8", null ],
+    [ "setNumberStyle", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a93c191ad93349d1e9c6c8851d41a3316", null ],
+    [ "setPrefix", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#aee2e036151f5fe6168cd87acc39ccc54", null ],
+    [ "setStartPageIndex", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a3668dc58e3dbf153c4d91f7c2e2d6c81", null ],
+    [ "toString", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_label.html#a4e60efa0aab5c3a7f88bd0b0c60bb2d3", null ]
+];
