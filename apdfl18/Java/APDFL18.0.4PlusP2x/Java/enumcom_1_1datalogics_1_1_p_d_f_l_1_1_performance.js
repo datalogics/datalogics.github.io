7efabd9468fd30a608a35a375a23ec89c6eff458
Bruce Hoffman <bruceh@datalogics.com>
2024-06-04 09:45:54 -0500

    Sync Develop to main (#123)
    * Add C++, .NET, .NETFramework, and Java documentation for APDFL18.0.4PlusP2x'
    
    * APDFL18.0.4PlusP2x files
    
    * update to v18.0.4P2x Release_Notes.html
    
    * Second Update to the v18.0.4P2x Release_Notes.html
    
    ---------
    
    Co-authored-by: Richard Gailiunas <107433294+datalogics-rgailiunas@users.noreply.github.com>
    Co-authored-by: datalogics-joffref <joffref@datalogics.com>
    Co-authored-by: datalogics-joffref <87712425+datalogics-joffref@users.noreply.github.com>

---
+++ apdfl18/Java/APDFL18.0.4PlusP2x/Java/enumcom_1_1datalogics_1_1_p_d_f_l_1_1_performance.js
@@ -0,0 +1,7 @@
+var enumcom_1_1datalogics_1_1_p_d_f_l_1_1_performance =
+[
+    [ "BEST_ACCURACY", "enumcom_1_1datalogics_1_1_p_d_f_l_1_1_performance.html#a77486066132b2b997bd3f1f399e4d643", null ],
+    [ "DEFAULT", "enumcom_1_1datalogics_1_1_p_d_f_l_1_1_performance.html#a8cfb53ecf22c18d98ea911f84fb213ca", null ],
+    [ "FASTER", "enumcom_1_1datalogics_1_1_p_d_f_l_1_1_performance.html#af580e2fad07fc1f55934c31a2bbb98e2", null ],
+    [ "MORE_ACCURACY", "enumcom_1_1datalogics_1_1_p_d_f_l_1_1_performance.html#a0d8e6e9fd223aac80836457a60bc4126", null ]
+];
