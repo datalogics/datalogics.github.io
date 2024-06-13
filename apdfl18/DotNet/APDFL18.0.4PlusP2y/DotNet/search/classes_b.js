136b368730ed750be3e0a6a2c85c0286512a7b1f
Arnulfo Arroyo <aarroyo@datalogics.com>
2024-06-13 12:09:31 -0500

    Add .NET .NETFramework Java documentation for P2y

---
+++ apdfl18/DotNet/APDFL18.0.4PlusP2y/DotNet/search/classes_b.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['namedcolorspace_2112',['NamedColorSpace',['../class_datalogics_1_1_p_d_f_l_1_1_named_color_space.html',1,'Datalogics::PDFL']]],
+  ['namedfunction_2113',['NamedFunction',['../class_datalogics_1_1_p_d_f_l_1_1_named_function.html',1,'Datalogics::PDFL']]],
+  ['nametree_2114',['NameTree',['../class_datalogics_1_1_p_d_f_l_1_1_name_tree.html',1,'Datalogics::PDFL']]],
+  ['numbertree_2115',['NumberTree',['../class_datalogics_1_1_p_d_f_l_1_1_number_tree.html',1,'Datalogics::PDFL']]]
+];
---
+++ apdfl18/DotNetFramework/APDFL18.0.4PlusP2y/DotNetFramework/search/files_15.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['watermarkdrawoption_2ecs_2425',['WatermarkDrawOption.cs',['../_watermark_draw_option_8cs.html',1,'']]],
+  ['watermarkparams_2ecs_2426',['WatermarkParams.cs',['../_watermark_params_8cs.html',1,'']]],
+  ['watermarktextparams_2ecs_2427',['WatermarkTextParams.cs',['../_watermark_text_params_8cs.html',1,'']]],
+  ['word_2ecs_2428',['Word.cs',['../_word_8cs.html',1,'']]],
+  ['wordattributeflags_2ecs_2429',['WordAttributeFlags.cs',['../_word_attribute_flags_8cs.html',1,'']]],
+  ['wordfinder_2ecs_2430',['WordFinder.cs',['../_word_finder_8cs.html',1,'']]],
+  ['wordfinderconfig_2ecs_2431',['WordFinderConfig.cs',['../_word_finder_config_8cs.html',1,'']]],
+  ['wordfinderversion_2ecs_2432',['WordFinderVersion.cs',['../_word_finder_version_8cs.html',1,'']]],
+  ['wordproc_2ecs_2433',['WordProc.cs',['../_word_proc_8cs.html',1,'']]],
+  ['writingmode_2ecs_2434',['WritingMode.cs',['../_writing_mode_8cs.html',1,'']]]
+];
