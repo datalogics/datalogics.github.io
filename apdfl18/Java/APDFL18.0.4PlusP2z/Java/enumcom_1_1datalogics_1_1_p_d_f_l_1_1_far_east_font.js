0e7d1f2ced9a2d0f45113d857118bf2f4f2007a3
bruceh <bruceh@datalogics.com>
2024-06-25 17:32:22 -0500

    Add P2z files

---
+++ apdfl18/Java/APDFL18.0.4PlusP2z/Java/enumcom_1_1datalogics_1_1_p_d_f_l_1_1_far_east_font.js
@@ -0,0 +1,6 @@
+var enumcom_1_1datalogics_1_1_p_d_f_l_1_1_far_east_font =
+[
+    [ "DOWNLOAD_ALL", "enumcom_1_1datalogics_1_1_p_d_f_l_1_1_far_east_font.html#a6bc15223e953c935675e49f7cc2cfd9b", null ],
+    [ "DOWNLOAD_NONE", "enumcom_1_1datalogics_1_1_p_d_f_l_1_1_far_east_font.html#ae6e3efa4e408ac397299c169e08eb65f", null ],
+    [ "PRINT_AS_IMAGE", "enumcom_1_1datalogics_1_1_p_d_f_l_1_1_far_east_font.html#aa889902dec6f41a2b4a1eae48cd9de97", null ]
+];
