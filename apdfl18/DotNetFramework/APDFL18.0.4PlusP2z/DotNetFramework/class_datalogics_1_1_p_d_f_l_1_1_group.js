08c2f01abf43b231211e9635141ba54ee73fa7a2
Bruce Hoffman <bruceh@datalogics.com>
2024-06-25 19:09:24 -0500

    Add C++, .NET, .NETFramework, and Java documentation for APDFL18.0.4PlusP2z (#128)
    * Add .NET .NET Framework Java documentation for P2z
    
    * Add P2z files

---
+++ apdfl18/DotNetFramework/APDFL18.0.4PlusP2z/DotNetFramework/class_datalogics_1_1_p_d_f_l_1_1_group.js
@@ -0,0 +1,17 @@
+var class_datalogics_1_1_p_d_f_l_1_1_group =
+[
+    [ "Group", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a302bad205b8695e7c0bb502ca3c28225", null ],
+    [ "Group", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a3942afa296cb6f47b3c1729b201e79ef", null ],
+    [ "Clone", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a131d5cee7f82d7ae9c676f1e6863fd4c", null ],
+    [ "Clone", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a7952e6a94a36c0726a7151442a665e31", null ],
+    [ "ColorConvert", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a3144816246b65a0ca699b8642684fc4c", null ],
+    [ "Dispose", "class_datalogics_1_1_p_d_f_l_1_1_group.html#aac97ffd66e9328f0a621ce862bcf3f2a", null ],
+    [ "Dispose", "class_datalogics_1_1_p_d_f_l_1_1_group.html#abec6c8356a1e84156ff9b63e2352a184", null ],
+    [ "Rotate", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a314c0603bad2cb15c49725bb7817a4cb", null ],
+    [ "Scale", "class_datalogics_1_1_p_d_f_l_1_1_group.html#af5fc1eaee42d45ba85162e31e45ee4f0", null ],
+    [ "Translate", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a90b055f53ac41dcde1a9dd1a9e11b15e", null ],
+    [ "BoundingBox", "class_datalogics_1_1_p_d_f_l_1_1_group.html#af9afded4c8f29a0a73d249950e8b9ac2", null ],
+    [ "Clip", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a6bd82950d730a84d2f335f520599328d", null ],
+    [ "Content", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a43518bbf5ff7caedc2fd90094f30cf38", null ],
+    [ "Matrix", "class_datalogics_1_1_p_d_f_l_1_1_group.html#a74efccd68b2554c3eb4146b25ff306f3", null ]
+];
---
+++ apdfl18/DotNetFramework/APDFL18.0.4PlusP2z/DotNetFramework/search/enums_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['numberstyle_2846',['NumberStyle',['../namespace_datalogics_1_1_p_d_f_l.html#a23b2c91cfb6ead7164cc0eca731a90d9',1,'Datalogics::PDFL']]]
+];
