73dd226aed7bfc7d072cee6c98b02e3d108d15e0
devauto <devauto@datalogics.com>
2024-12-03 10:57:38 -0600

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1e

---
+++ apdfl18/DotNetFramework/APDFL18.0.5PlusP1e/DotNetFramework/navtreedata.js
@@ -0,0 +1,69 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ ".NET Framework API Reference", "index.html", [
+    [ "Instantiating the Library", "index.html#instantiating", null ],
+    [ "Activating a License-Managed version", "index.html#activating", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Properties", "functions_prop.html", "functions_prop" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"class_datalogics_1_1_p_d_f_l_1_1_color_space.html#a4c78a74177f7f07cde09dfda7cc3a25e",
+"class_datalogics_1_1_p_d_f_l_1_1_document.html#ae2bb604500207fee52efd30d222c7543",
+"class_datalogics_1_1_p_d_f_l_1_1_graphic_state.html#ab363c36c50a204301033e7056352c019",
+"class_datalogics_1_1_p_d_f_l_1_1_library.html#a6696864b6838bef2920f770140277520",
+"class_datalogics_1_1_p_d_f_l_1_1_optional_content_order_leaf.html#a481e4af1fc98d04a5c36ba9223659db0",
+"class_datalogics_1_1_p_d_f_l_1_1_p_d_f_string.html#a9552b6a3701a12ba5b7a8a4dbf04f017",
+"class_datalogics_1_1_p_d_f_l_1_1_post_script_calculator_function.html#a57d0cc4c9ee1c23cc37e0d6b2965ffa3",
+"class_datalogics_1_1_p_d_f_l_1_1_redaction.html#ad254d6966a8aaa69b6ccf5ff4be9b1f5",
+"class_datalogics_1_1_p_d_f_l_1_1_text_state.html#a463d8d895e437a407cc6d6266cff4635",
+"namespace_datalogics_1_1_p_d_f_l.html#a125c40986c7bea800ccd5124fab36af1a2570f2c64c1c28a134ae86d9747a3aef",
+"namespace_datalogics_1_1_p_d_f_l.html#a3b5c28eeaf868853c6ce91a898558d8aa8204aa7471d903e8a07537e26d78b2c7",
+"namespace_datalogics_1_1_p_d_f_l.html#a704fd3fd78cb70455a28cc16b39955e8a17672567681d5bff9f9d0cb1cc18f503",
+"namespace_datalogics_1_1_p_d_f_l.html#abfbe3bc9bd69bee0f109f8390f4509a5ac912160a56399120050bf15b80ef84cf"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ apdfl18/Java/APDFL18.0.5PlusP1e/Java/search/classes_9.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['labcolorspace_2556',['LabColorSpace',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_lab_color_space.html',1,'com::datalogics::PDFL']]],
+  ['language_2557',['Language',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_language.html',1,'com::datalogics::PDFL']]],
+  ['languagesetting_2558',['LanguageSetting',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_language_setting.html',1,'com::datalogics::PDFL']]],
+  ['launchaction_2559',['LaunchAction',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_launch_action.html',1,'com::datalogics::PDFL']]],
+  ['library_2560',['Library',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_library.html',1,'com::datalogics::PDFL']]],
+  ['libraryexception_2561',['LibraryException',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_library_exception.html',1,'com::datalogics::PDFL']]],
+  ['libraryflags_2562',['LibraryFlags',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_library_flags.html',1,'com::datalogics::PDFL']]],
+  ['lineannotation_2563',['LineAnnotation',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_line_annotation.html',1,'com::datalogics::PDFL']]],
+  ['linecap_2564',['LineCap',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_line_cap.html',1,'com::datalogics::PDFL']]],
+  ['lineendingstyle_2565',['LineEndingStyle',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_line_ending_style.html',1,'com::datalogics::PDFL']]],
+  ['linejoin_2566',['LineJoin',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_line_join.html',1,'com::datalogics::PDFL']]],
+  ['lineto_2567',['LineTo',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_line_to.html',1,'com::datalogics::PDFL']]],
+  ['linkannotation_2568',['LinkAnnotation',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_link_annotation.html',1,'com::datalogics::PDFL']]],
+  ['logoutputflags_2569',['LogOutputFlags',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_log_output_flags.html',1,'com::datalogics::PDFL']]],
+  ['logparams_2570',['LogParams',['../classcom_1_1datalogics_1_1_p_d_f_l_1_1_log_params.html',1,'com::datalogics::PDFL']]]
+];
