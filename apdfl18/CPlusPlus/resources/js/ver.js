556f4dbb1204e3a40042d3902400418080b60337
devauto <devauto@datalogics.com>
2025-02-04 14:43:20 -0600

    P1g update base files

--- apdfl18/CPlusPlus/resources/js/ver.js
+++ apdfl18/CPlusPlus/resources/js/ver.js
@@ -18,7 +18,7 @@ function goToPage(ver) {
 } 
 
 function changeResult(lang) {
-var verDict = ['APDFL18.0.5PlusP1f', 'APDFL18.0.5PlusP1e', 'APDFL18.0.5PlusP1d', 'APDFL18.0.5PlusP1c']
+var verDict = ['APDFL18.0.5PlusP1g', 'APDFL18.0.5PlusP1f', 'APDFL18.0.5PlusP1e', 'APDFL18.0.5PlusP1d']
     selct = document.getElementById("verDrop")
     selct.textContent = ''
     selct.appendChild(new Option("-"))
