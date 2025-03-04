2a43238ab0162825c0f9fd50759424d2b7a2b6e0
devauto <devauto@datalogics.com>
2025-03-04 13:03:30 -0600

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1h

---
+++ apdfl18/DotNet/APDFL18.0.5PlusP1h/search/files_4.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['element_2ecs_2246',['Element.cs',['../_element_8cs.html',1,'']]],
+  ['elementcloneflags_2ecs_2247',['ElementCloneFlags.cs',['../_element_clone_flags_8cs.html',1,'']]],
+  ['embedflags_2ecs_2248',['EmbedFlags.cs',['../_embed_flags_8cs.html',1,'']]],
+  ['encodinginfo_2ecs_2249',['EncodingInfo.cs',['../_encoding_info_8cs.html',1,'']]],
+  ['encryptiontype_2ecs_2250',['EncryptionType.cs',['../_encryption_type_8cs.html',1,'']]],
+  ['exponentialfunction_2ecs_2251',['ExponentialFunction.cs',['../_exponential_function_8cs.html',1,'']]],
+  ['extendedgraphicstate_2ecs_2252',['ExtendedGraphicState.cs',['../_extended_graphic_state_8cs.html',1,'']]]
+];
---
+++ apdfl18/DotNetFramework/APDFL18.0.5PlusP1h/search/files_f.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['quad_2ecs_2394',['Quad.cs',['../_quad_8cs.html',1,'']]]
+];
