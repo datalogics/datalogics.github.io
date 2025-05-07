34dbfbed57c8f11658969d77a59502cc32b72f3d
devauto <devauto@datalogics.com>
2025-05-07 12:35:19 -0500

    P1m update base files

--- apdfl18/CPlusPlus/resources/js/ver.js
+++ apdfl18/CPlusPlus/resources/js/ver.js
@@ -18,7 +18,7 @@ function goToPage(ver) {
 } 
 
 function changeResult(lang) {
-var verDict = ['APDFL18.0.5PlusP1k', 'APDFL18.0.5PlusP1h', 'APDFL18.0.5PlusP1g', 'APDFL18.0.5PlusP1f']
+var verDict = ['APDFL18.0.5PlusP1m', 'APDFL18.0.5PlusP1k', 'APDFL18.0.5PlusP1h', 'APDFL18.0.5PlusP1g']
     selct = document.getElementById("verDrop")
     selct.textContent = ''
     selct.appendChild(new Option("-"))
