bc856029cb75738df3c3142f1fac89d7b1ea87ae
devauto <devauto@datalogics.com>
2025-07-23 11:21:53 -0500

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1r

---
+++ apdfl18/DotNetFramework/APDFL18.0.5PlusP1r/search/all_17.js
@@ -0,0 +1,19 @@
+var searchData=
+[
+  ['x1a_2100',['X1a',['../namespace_datalogics_1_1_p_d_f_l.html#a75abc313ee2b9c29c39c0e0a690e1716a6029d22f5d0af2062a154c7102c6353b',1,'Datalogics::PDFL']]],
+  ['x3_2101',['X3',['../namespace_datalogics_1_1_p_d_f_l.html#a75abc313ee2b9c29c39c0e0a690e1716a0541026d4fa57ca1cc1a6fe30f57fea1',1,'Datalogics::PDFL']]],
+  ['x4_2102',['X4',['../namespace_datalogics_1_1_p_d_f_l.html#a75abc313ee2b9c29c39c0e0a690e1716a7d71ed2af4cc5c6a8380324d9bc4a45f',1,'Datalogics::PDFL']]],
+  ['x6_2103',['X6',['../namespace_datalogics_1_1_p_d_f_l.html#a75abc313ee2b9c29c39c0e0a690e1716adf2412d1342fd25951cfc44de6f49acd',1,'Datalogics::PDFL']]],
+  ['xdp_2104',['XDP',['../namespace_datalogics_1_1_p_d_f_l.html#aa3dc93bd5702e4cc157403f98aa66e91af7608b81a83cba5be24044b16d192730',1,'Datalogics::PDFL']]],
+  ['xfaformexporttype_2105',['XFAFormExportType',['../namespace_datalogics_1_1_p_d_f_l.html#aa3dc93bd5702e4cc157403f98aa66e91',1,'Datalogics::PDFL']]],
+  ['xfaformexporttype_2ecs_2106',['XFAFormExportType.cs',['../_x_f_a_form_export_type_8cs.html',1,'']]],
+  ['xfd_2107',['XFD',['../namespace_datalogics_1_1_p_d_f_l.html#aa3dc93bd5702e4cc157403f98aa66e91a54b7d5ea58bcefcc3c757c5461729f5c',1,'Datalogics::PDFL']]],
+  ['xfdf_2108',['XFDF',['../namespace_datalogics_1_1_p_d_f_l.html#a5f1b6004c4fd54b535e4be15e6e5f5bbacf98ca59c083f14c3117d8db340e4933',1,'Datalogics.PDFL.XFDF()'],['../namespace_datalogics_1_1_p_d_f_l.html#a0d55354a5951840ea48937706d0c8cf1acf98ca59c083f14c3117d8db340e4933',1,'Datalogics.PDFL.XFDF()']]],
+  ['xheight_2109',['XHeight',['../class_datalogics_1_1_p_d_f_l_1_1_font.html#a6ec405e4bf25150bf6f5b372b5cf0234',1,'Datalogics::PDFL::Font']]],
+  ['xml_2110',['XML',['../namespace_datalogics_1_1_p_d_f_l.html#a5f1b6004c4fd54b535e4be15e6e5f5bba3501bb093d363810b671059b9cfed3f8',1,'Datalogics.PDFL.XML()'],['../namespace_datalogics_1_1_p_d_f_l.html#a0d55354a5951840ea48937706d0c8cf1a3501bb093d363810b671059b9cfed3f8',1,'Datalogics.PDFL.XML()'],['../namespace_datalogics_1_1_p_d_f_l.html#aa3dc93bd5702e4cc157403f98aa66e91a3501bb093d363810b671059b9cfed3f8',1,'Datalogics.PDFL.XML()']]],
+  ['xmpmetadata_2111',['XMPMetadata',['../class_datalogics_1_1_p_d_f_l_1_1_document.html#add3ab7e0fbed4e50358db31d54a6c900',1,'Datalogics.PDFL.Document.XMPMetadata()'],['../class_datalogics_1_1_p_d_f_l_1_1_p_d_f_dict.html#a8c3b4ed4d521cab6634560ada23f5ae4',1,'Datalogics.PDFL.PDFDict.XMPMetadata()']]],
+  ['xmppacketreadonlynopadding_2112',['XMPPacketReadOnlyNoPadding',['../namespace_datalogics_1_1_p_d_f_l.html#a8eea9060907e36311340a42971318c3da53fda411dca70db363c7a873a85310ea',1,'Datalogics::PDFL']]],
+  ['xobject_2113',['XObject',['../namespace_datalogics_1_1_p_d_f_l.html#a9b5401b8ab857a0eb90a503b8381e7b7a4ad979e38265904425a8b6dff6a70938',1,'Datalogics::PDFL']]],
+  ['xpsconvertparams_2114',['XPSConvertParams',['../class_datalogics_1_1_p_d_f_l_1_1_x_p_s_convert_params.html',1,'Datalogics.PDFL.XPSConvertParams'],['../class_datalogics_1_1_p_d_f_l_1_1_x_p_s_convert_params.html#afc0e115ed296f34c330290dcac8007f6',1,'Datalogics.PDFL.XPSConvertParams.XPSConvertParams()'],['../class_datalogics_1_1_p_d_f_l_1_1_x_p_s_convert_params.html#a227b5e14d99a2a367c8ff07a5eddffcf',1,'Datalogics.PDFL.XPSConvertParams.XPSConvertParams(string SettingsFile)']]],
+  ['xpsconvertparams_2ecs_2115',['XPSConvertParams.cs',['../_x_p_s_convert_params_8cs.html',1,'']]]
+];
---
+++ apdfl18/Java/APDFL18.0.5PlusP1r/classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.js
@@ -0,0 +1,11 @@
+var classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder =
+[
+    [ "DocTextFinder", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#a2269146909fbe5e628f3928600cdc558", null ],
+    [ "DocTextFinder", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#a02e3845ee7069b76ec9374b529e350f0", null ],
+    [ "DocTextFinder", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#aef204e3faa017ea84e05665d1df525fd", null ],
+    [ "delete", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#a3e801836a70ba61aa65fa693ddc39e6b", null ],
+    [ "delete", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#ad3bc9c169571b4c08c43ad8717f3e72c", null ],
+    [ "finalize", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#a3af741c49c1777723753cb42bf4ae149", null ],
+    [ "getCPtr", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#af25b67f3d43cebe45f6f5d9bfdd8a31e", null ],
+    [ "getMatchList", "classcom_1_1datalogics_1_1_p_d_f_l_1_1_doc_text_finder.html#a7a6bc8504121a36cd04bdb2aa53da581", null ]
+];
