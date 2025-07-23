d33db9c75a235603f377682420e559d193007be7
devauto <devauto@datalogics.com>
2025-07-23 11:21:53 -0500

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1r

---
+++ apdfl18/Java/APDFL18.0.5PlusP1r/navtreedata.js
@@ -0,0 +1,69 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Java API Reference", "index.html", [
+    [ "Instantiating the Library", "index.html#instantiating", null ],
+    [ "Activating a License-Managed version", "index.html#activating", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_collection_schema.html#a3fe7d5e23ff5e90ffd9543ed84a2137f",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_device_n_color_space.html#a47cfbe82674b692332949b0fa2249c4b",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_draw_params.html#a9815529e0f7b85fcca693cd2bba128b3",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_free_text_annotation.html#a2a1c1ac4b655e4be279b62935ebf5849",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_image_data.html#a3a349ed148f2c98afe5c932698e4bbab",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_line_annotation.html#a768e1acaa49cffb963dfc81154f5a80d",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_optional_content_config.html#a159dc1876fd5d8e03ec888078f0e06bc",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_p_d_f_name.html#a7355bc3b1fd4b345190931bdc8139c17",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_page_image_params.html#a5949f3473b47a9f5b6562a4b98d47237",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_post_script.html#aecd44541fdb6de921481d4479549e102",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_print_user_params.html#adbfdd56812c474fb4114bb7b8e4b4e50",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_segment.html",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_text.html#a634c362f44005bdea49bf8fd030ef7d8",
+"classcom_1_1datalogics_1_1_p_d_f_l_1_1_word_finder.html#a938187d5c4e2cb4e0e8de85dda600cd5",
+"enumcom_1_1datalogics_1_1_p_d_f_l_1_1_draw_flags.html#a61512d5e6ffa47e1975a6a3d10a9ecad",
+"enumcom_1_1datalogics_1_1_p_d_f_l_1_1_line_ending_style.html",
+"enumcom_1_1datalogics_1_1_p_d_f_l_1_1_permission_flags.html#a37d155579a80e316163d47dfb138c7bd",
+"enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_finder_version.html#a0065a7cc5519682b75b236c11aefc0b6"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ apdfl18/Java/APDFL18.0.5PlusP1r/search/variables_17.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['x1_5fa_5233',['X1_A',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_p_d_f_x_convert_type.html#a3228a0b6dca69e69830626c996c030a6',1,'com::datalogics::PDFL::PDFXConvertType']]],
+  ['x3_5234',['X3',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_p_d_f_x_convert_type.html#a5378cc0447831ebdbab818b84d91ae13',1,'com::datalogics::PDFL::PDFXConvertType']]],
+  ['x4_5235',['X4',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_p_d_f_x_convert_type.html#ab2a9bb28a2196d512958880d96368f88',1,'com::datalogics::PDFL::PDFXConvertType']]],
+  ['x6_5236',['X6',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_p_d_f_x_convert_type.html#a8882b6010683b4498fd66bc6ae828144',1,'com::datalogics::PDFL::PDFXConvertType']]],
+  ['xdp_5237',['XDP',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_x_f_a_form_export_type.html#affc514cf220fa7f566803ca0238317f1',1,'com::datalogics::PDFL::XFAFormExportType']]],
+  ['xfd_5238',['XFD',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_x_f_a_form_export_type.html#aeccdbf5938ea72c3ab569e2c20e77b4c',1,'com::datalogics::PDFL::XFAFormExportType']]],
+  ['xfdf_5239',['XFDF',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_acro_form_export_type.html#aee049d5e8c37e7a4fec4a726162502b2',1,'com.datalogics.PDFL.AcroFormExportType.XFDF()'],['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_acro_form_import_type.html#a425ee0e8f87444abb97ebb2e1abc4ff0',1,'com.datalogics.PDFL.AcroFormImportType.XFDF()']]],
+  ['xml_5240',['XML',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_acro_form_export_type.html#adb5fec50e1a95aa941bf06126fc2e4b8',1,'com.datalogics.PDFL.AcroFormExportType.XML()'],['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_acro_form_import_type.html#aa0980fec4da99167b26eec22b1c1cffb',1,'com.datalogics.PDFL.AcroFormImportType.XML()'],['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_x_f_a_form_export_type.html#a54a01ea9f48c0a7b0f8b16d9f93ed9b1',1,'com.datalogics.PDFL.XFAFormExportType.XML()']]],
+  ['xmppacket_5fread_5fonly_5fno_5fpadding_5241',['XMPPACKET_READ_ONLY_NO_PADDING',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_save_flags.html#a3fe16215073210ba420e2c84300d5309',1,'com::datalogics::PDFL::SaveFlags']]],
+  ['xobject_5242',['XOBJECT',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_resource_type.html#a42793ab4a0aeb7cb6af0a4d4768a8ca7',1,'com::datalogics::PDFL::ResourceType']]]
+];
