7a9bf612adeebf17f8876e3b3faf2ae5feb2c820
devauto <devauto@datalogics.com>
2025-07-23 11:21:53 -0500

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1r

--- apdfl18/DotNetFramework/APDFL18.0.5PlusP1k/search/classes_12.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['underlineannotation_2186',['UnderlineAnnotation',['../class_datalogics_1_1_p_d_f_l_1_1_underline_annotation.html',1,'Datalogics::PDFL']]],
-  ['uriaction_2187',['URIAction',['../class_datalogics_1_1_p_d_f_l_1_1_u_r_i_action.html',1,'Datalogics::PDFL']]]
-];
