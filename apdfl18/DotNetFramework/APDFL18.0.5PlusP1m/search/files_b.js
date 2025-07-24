e1fd8da0e3e7fe02203b8af93786304389e2fbfe
devauto <devauto@datalogics.com>
2025-07-24 12:42:46 -0500

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1s

--- apdfl18/DotNetFramework/APDFL18.0.5PlusP1m/search/files_b.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['matrix_2ecs_2366',['Matrix.cs',['../_matrix_8cs.html',1,'']]],
-  ['mirroroptions_2ecs_2367',['MirrorOptions.cs',['../_mirror_options_8cs.html',1,'']]],
-  ['missingfontstrategy_2ecs_2368',['MissingFontStrategy.cs',['../_missing_font_strategy_8cs.html',1,'']]],
-  ['moveto_2ecs_2369',['MoveTo.cs',['../_move_to_8cs.html',1,'']]]
-];
