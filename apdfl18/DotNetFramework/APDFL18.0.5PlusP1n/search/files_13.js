6d2b4f8ed621cf5b8faab65701245586b6e0bb4e
devauto <devauto@datalogics.com>
2025-09-02 14:50:32 -0500

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1t

--- apdfl18/DotNetFramework/APDFL18.0.5PlusP1n/search/files_13.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['underlineannotation_2ecs_2492',['UnderlineAnnotation.cs',['../_underline_annotation_8cs.html',1,'']]],
-  ['uriaction_2ecs_2493',['URIAction.cs',['../_u_r_i_action_8cs.html',1,'']]]
-];
---
+++ apdfl18/DotNetFramework/APDFL18.0.5PlusP1t/search/enums_e.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['saveflags_2997',['SaveFlags',['../namespace_datalogics_1_1_p_d_f_l.html#a8eea9060907e36311340a42971318c3d',1,'Datalogics::PDFL']]],
+  ['schemafieldsubtype_2998',['SchemaFieldSubtype',['../namespace_datalogics_1_1_p_d_f_l.html#ab2436efbaf12725e689703856752c215',1,'Datalogics::PDFL']]],
+  ['separationfontoptions_2999',['SeparationFontOptions',['../namespace_datalogics_1_1_p_d_f_l.html#a35e66fe395572b27763ac495d4f28ed8',1,'Datalogics::PDFL']]],
+  ['separationpreviewoptions_3000',['SeparationPreviewOptions',['../namespace_datalogics_1_1_p_d_f_l.html#a7efb402837c4569bb6fdc7e4377d7150',1,'Datalogics::PDFL']]],
+  ['signaturefieldid_3001',['SignatureFieldID',['../namespace_datalogics_1_1_p_d_f_l.html#aee1637f86cac7f765b9e7c574c7f6424',1,'Datalogics::PDFL']]],
+  ['smoothflags_3002',['SmoothFlags',['../namespace_datalogics_1_1_p_d_f_l.html#a847efbb0b99b5632923c2009397cf771',1,'Datalogics::PDFL']]]
+];
