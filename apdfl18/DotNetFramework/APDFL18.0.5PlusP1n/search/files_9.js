982a043b9212cf5d39cd5ae57ba4dba0ce5e1bf5
devauto <devauto@datalogics.com>
2025-09-02 16:08:26 -0500

    Add C++, .NET, .NETFrwk, and Java documentation for APDFL18.0.5PlusP1t

--- apdfl18/DotNetFramework/APDFL18.0.5PlusP1n/search/files_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['javastreamfile_2ecs_2350',['JavaStreamFile.cs',['../_java_stream_file_8cs.html',1,'']]]
-];
---
+++ apdfl18/Java/APDFL18.0.5PlusP1t/search/variables_7.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['haitian_4766',['HAITIAN',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_language.html#ac7c9f1a0a3b4a0f86c98aba71591d9ff',1,'com::datalogics::PDFL::Language']]],
+  ['hard_5flight_4767',['HARD_LIGHT',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_blend_mode.html#a9f90737d79e35854b8092e286c629994',1,'com::datalogics::PDFL::BlendMode']]],
+  ['has_5fdigit_4768',['HAS_DIGIT',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a834667c39eede1db7962ed954191baf1',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fhyphen_4769',['HAS_HYPHEN',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a1355753fcde15ddad4d817a52a7a8cfc',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fleading_5fpunctuation_4770',['HAS_LEADING_PUNCTUATION',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a55e315ab09b7d8bc498e262ffd9ddce3',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fletter_4771',['HAS_LETTER',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a2fb1d7120750bb9bc70a85c2b2dce650',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fligature_4772',['HAS_LIGATURE',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a06f3b793d6cc1b0ce43c91d4078a4025',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fnonalphanum_4773',['HAS_NONALPHANUM',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#ad96fecf17ee5cea9090938e438119709',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fpunctuation_4774',['HAS_PUNCTUATION',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a9a582759e54f78c8e0297b842445a842',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fsoft_5fhyphen_4775',['HAS_SOFT_HYPHEN',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a381dd296cff2fc782eff4ca3a76cefcc',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5ftrailing_5fpunctuation_4776',['HAS_TRAILING_PUNCTUATION',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#afa55277fba466ce3a4dde4b42badfe92',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5funmapped_5fcharacters_4777',['HAS_UNMAPPED_CHARACTERS',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#a1e598b03192dbbbe62af57158c1cd0c0',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['has_5fuppercase_4778',['HAS_UPPERCASE',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_word_attribute_flags.html#aeb22af908e5bdca29f94b019734bab62',1,'com::datalogics::PDFL::WordAttributeFlags']]],
+  ['hidden_4779',['HIDDEN',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_annotation_flags.html#a8e1d6b9b77660b5682c3695034d3e04a',1,'com::datalogics::PDFL::AnnotationFlags']]],
+  ['high_4780',['HIGH',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_compress_quality.html#afc72fa3a10a2ee7bb6a5cd6e9a83b2f4',1,'com.datalogics.PDFL.CompressQuality.HIGH()'],['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_flattener_quality_setting.html#acbfdc6bb948fffb5d65c6c926e67470b',1,'com.datalogics.PDFL.FlattenerQualitySetting.HIGH()']]],
+  ['high_5fprint_4781',['HIGH_PRINT',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_permission_flags.html#a3fb45caedb36022ec7088a58d407ecd4',1,'com::datalogics::PDFL::PermissionFlags']]],
+  ['hindi_4782',['HINDI',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_language.html#a2a4d404cf18152fcb15ed1ffe0a40d99',1,'com::datalogics::PDFL::Language']]],
+  ['horizontal_4783',['HORIZONTAL',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_writing_mode.html#a8343f9b059de105b06d939549a2fe711',1,'com::datalogics::PDFL::WritingMode']]],
+  ['host_4784',['HOST',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_t_i_f_f_byte_order.html#add5ef357cae09025751469ed198ebf7f',1,'com::datalogics::PDFL::TIFFByteOrder']]],
+  ['hue_4785',['HUE',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_blend_mode.html#a802a41b4e9bb1f419958f6a269de523d',1,'com::datalogics::PDFL::BlendMode']]],
+  ['hungarian_4786',['HUNGARIAN',['../enumcom_1_1datalogics_1_1_p_d_f_l_1_1_language.html#afe005121c7588720bb0d2e879d13595e',1,'com::datalogics::PDFL::Language']]]
+];
